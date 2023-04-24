d3f20c2fdeedad3809acd46c4f4aaeb98c18b950
leo23225 <leo_94310@hotmail.com>
2023-04-24 15:04:28 +0200

    Etape 2 IMPORTER LES DATAS + Etape 3

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,9 +1,10 @@
     async function getPhotographers() {
         // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
+
         let photographers = [
             {
-                "name": "Ma data test",
+                "name": "1er photogrpahe",
                 "id": 1,
                 "city": "Paris",
                 "country": "France",
